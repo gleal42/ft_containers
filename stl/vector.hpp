e236725a350b2c05ab5332abf684560262669c4b
gleal42 <jobsgoncaloleal@gmail.com>
2022-06-08 17:14:10 +0100

    Added more tests, implemented iterator functions. Started added Capacity Functions.

--- stl/vector.hpp
+++ stl/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: gleal <gleal@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 21:34:59 by gleal             #+#    #+#             */
-/*   Updated: 2022/06/06 01:40:04 by gleal            ###   ########.fr       */
+/*   Updated: 2022/06/08 17:11:32 by gleal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,12 +38,16 @@ namespace ft
             typedef typename std::size_t                                    size_type;
             /* ---------------------------- Member Functions ---------------------------- */
             // vector(); //(1) (not clear if C++98)
+
+			/* ------------------------------ (constructor) ----------------------------- */
 			explicit vector (const allocator_type& alloc = allocator_type())
 			: _start(0), _finish(0), _end_of_storage(0), _alloc(alloc)
 			{
+				// std::cout << "default constructor called" << std::endl;
 			}
 			explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()) : _alloc(alloc)
 			{
+				// std::cout << "fill constructor called" << std::endl;
 				_start = _alloc.allocate(n);
 				_finish = _start;
 				for (size_type vec_size = 0; vec_size < n; vec_size++)
@@ -55,14 +59,16 @@ namespace ft
 			}
 			template < class InputIterator>
 			vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(),
-			typename enable_if<!is_integral<InputIterator>::value >::type* = 0) : _alloc(alloc)
+			typename enable_if<!is_integral<InputIterator>::value >::type* = 0)
+			: _alloc(alloc)
 			{
+				// std::cout << "range constructor called" << std::endl;
 				size_type range_size = last - first;
 				_start = _alloc.allocate(range_size);
 				_finish = _start;
 				while (first < last)
 				{
-					alloc.construct(_finish, *first);
+					_alloc.construct(_finish, *first);
 					_finish++;
 					first++;
 				}
@@ -70,6 +76,42 @@ namespace ft
 			}
 			vector (const vector& x)
 			{
+				std::cout << "copy constructor called" << std::endl;
+				_start = _alloc.allocate(x.capacity());
+				_finish = _start;
+				iterator x_begin = x.begin();
+				iterator x_end = x.end();
+				while (x_begin < x_end)
+				{
+					_alloc.construct(_finish, *x_begin);
+					_finish++;
+					x_begin++;
+				}
+				_end_of_storage = _start + x.capacity();
+			}
+			/* ------------------------------ (destructor) ------------------------------ */
+			~vector()
+			{
+				size_type count = _finish-_start;
+				while (_finish-_start > 0)
+				{
+					_alloc.destroy(_start);
+					_finish--;
+				}
+				_alloc.deallocate(_start, count);
+			}
+			/* -------------------------------- operator= ------------------------------- */
+
+			vector& operator= (const vector& x)
+			{
+				size_type count = _finish -_start;
+				while (_finish-_start > 0)
+				{
+					_alloc.destroy(_start);
+					_finish--;
+				}
+				_alloc.deallocate(_start, count);
+
 				_start = _alloc.allocate(x.capacity());
 				_finish = _start;
 				pointer x_begin = x.begin();
@@ -82,23 +124,63 @@ namespace ft
 				}
 				_end_of_storage = _start + x.capacity();
 			}
-			~vector()
-			{
-				size_type count = _finish-_start;
-				while (_finish-_start > 0)
-				{
-					_alloc.destroy(_start);
-					_finish--;
-				}
-				_alloc.deallocate(_start, count);
-			}
+
+			/* -------------------------------------------------------------------------- */
+			/*                                  Iterators                                 */
+			/* -------------------------------------------------------------------------- */
+
+			/* ---------------------------------- begin --------------------------------- */
 			iterator begin()
 			{
 				return (iterator(_start));
 			}
 			const_iterator begin() const
 			{
-				return (_start);
+				return (const_iterator(_start));
+			}
+			/* ----------------------------------- end ---------------------------------- */
+			iterator end()
+			{
+				return (iterator(_finish));
+			}
+			const_iterator end() const
+			{
+				return (const_iterator(_finish));
+			}
+			/* --------------------------------- rbegin --------------------------------- */
+			// reverse_iterator rbegin()
+			// {
+			// 	return (reverse_iterator(_start));
+			// }
+			// const_reverse_iterator rbegin() const
+			// {
+			// 	return (const_reverse_iterator(_start));
+			// }
+
+			/* ---------------------------------- rend ---------------------------------- */
+			// reverse_iterator rend()
+			// {
+			// 	return (reverse_iterator(_finish));
+			// }
+			// const_reverse_iterator rend() const
+			// {
+			// 	return (const_reverse_iterator(_finish));
+			// }
+	
+			/* -------------------------------------------------------------------------- */
+			/*                                  Capacity                                  */
+			/* -------------------------------------------------------------------------- */
+			size_type size() const
+			{
+				return (_finish - _start);
+			}
+			size_type max_size() const
+			{
+				return(_alloc.max_size());
+			}
+			void resize (size_type n, value_type val = value_type())
+			{
+				
 			}
         private:
             pointer		_start;
--- tests/srcs/test_vector.cpp
+++ tests/srcs/test_vector.cpp
@@ -6,11 +6,13 @@
 /*   By: gleal <gleal@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 21:42:39 by gleal             #+#    #+#             */
-/*   Updated: 2022/06/03 01:05:16 by gleal            ###   ########.fr       */
+/*   Updated: 2022/06/08 17:08:07 by gleal            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "all.hpp"
+
+#include <type_traits>
 
 // difference_type operator-(const Random_Access_Iterator &it) const;
 // friend difference_type operator-(const Random_Access_Iterator &a, const Random_Access_Iterator &b);
@@ -56,17 +58,8 @@ void	test_vector_iterator()
 }
 	// Where X is an iterator type, a and b are objects of this iterator type, t is an object of the type pointed by the iterator type, and n is an integer value.
 
-void    test_vector()
+void test_vec_42()
 {
-	ft::vector<std::string> vector_str;
-	ft::vector<int> vector_int;
-
-	ft::vector<int> multi_nbr(10, 5);
-
-	ft::vector<int>::iterator begin = multi_nbr.begin();
-
-	std::cout << *begin << std::endl;
-
 	// ft::vector<Buffer> vector_buffer;
     
 	// for (int i = 0; i < COUNT; i++)
@@ -93,6 +86,91 @@ void    test_vector()
 	// {
 	// 	//NORMAL ! :P
 	// }
+}
+
+void	test_vec_fts()
+{
+	// (constructor)
+	// default (1)	
+	// explicit vector (const allocator_type& alloc = allocator_type());
+
+		ft::vector<std::string> vector1;
+		ft::vector<int> vector2;
+		ft::vector<char> vector3;
+		ft::vector<char *> vector4;
+		ft::vector<const int> vector5;
+
+	// fill (2)	
+	// explicit vector (size_type n, const value_type& val = value_type(),
+	//                  const allocator_type& alloc = allocator_type());
+
+		ft::vector<std::string> vector6(5, "Hello");
+		// std::cout << std::is_const<ft::vector<std::string> >::value_type << std::endl;
+	
+		ft::vector<int> vector7(10, 5);
+		ft::vector<char> vector8(3, 'k');
+		char a[] = "Okay";
+		ft::vector<char *> vector9(4, a);
+		ft::vector<const int> vector10(2, 4);
+
+	// range (3)	
+	// template <class InputIterator>
+	//          vector (InputIterator first, InputIterator last,
+	//                  const allocator_type& alloc = allocator_type());
+
+		ft::vector<std::string> vector11(vector6.begin(), vector6.end());
+		ft::vector<int> vector12(vector7.begin(), vector7.end());
+		int myints[] = {16,2,77,29};
+		ft::vector<int> vector13 (myints, myints + sizeof(myints) / sizeof(int) );
+		ft::vector<char> vector14(vector8.begin(), vector8.end());
+		ft::vector<char *> vector15(vector9.begin(), vector9.end());
+		ft::vector<const int> vector16(vector10.begin(), vector10.end());
+
+	// copy (4)	
+	// vector (const vector& x);
+		ft::vector<std::string> vector17(vector11);
+		ft::vector<int> vector18(vector12);
+		ft::vector<int> vector19 (vector13);
+		ft::vector<char> vector20(vector14);
+		ft::vector<char *> vector21(vector15);
+		ft::vector<const int> vector22(vector16);
+
+	// (destructor)
+	// ~vector();
+
+	// operator=
+	// vector& operator= (const vector& x);
+
+	// empty = non-empty
+
+	// non-empty = empty
+	
+
+	// Iterators
+	// iterator begin();
+	
+	// const_iterator begin() const;
+	// Add error to check that std ft is being called
+
+
+	// Capacity
+	
+}
+
+void    test_vector()
+{
+	// test_vec_42();
+	test_vec_fts();
+	// main from 42
+
+	ft::vector<int> vector_int;
+
+	ft::vector<int> multi_nbr(10, 5);
+
+	ft::vector<int>::iterator begin = multi_nbr.begin();
+
+	std::cout << *begin << std::endl;
+
 	
 	test_vector_iterator();
 }
