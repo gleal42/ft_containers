6c6ecda24cf5ab9cca2b75de56be468f14135778
gleal42 <gonaloleal@gmail.com>
2023-04-07 12:44:22 +0100

    remove comments with todos

--- tests/srcs/test_vector.cpp
+++ tests/srcs/test_vector.cpp
@@ -13,60 +13,6 @@
 #include "all.hpp"
 #include <algorithm>
 
-/**
- * TODO: Random Access Iterator and Reverse Iterator Tests.
- */
-
-/**
- * TODO: Make tests to check if assign and assignment operator adjust capacity
- * if new contents are smaller or this resizing is only done in case the size
- * is not enough
- */
-
-// difference_type operator-(const Random_Access_Iterator &it) const;
-// friend difference_type operator-(const Random_Access_Iterator &a, const Random_Access_Iterator &b);
-// Can be dereferenced as an lvalue vs rvalue not clear implementation
-// Multi-Pass not clear implementation
-
-// void test_vector_iterator()
-// {
-	// ft::vector<int>::iterator a;
-	// ft::vector<int>::iterator b;
-
-	// std::cout << b - a << std::endl;
-
-	// std::cout << *(5 + a) << std::endl;
-
-	// X b(a);
-	// b = a;
-	// ++a
-	// a++
-	// a == b
-	// a != b
-	// *a
-	// a->m
-	// *a = t
-	// *a++ = t
-	// 	X a;
-	// X()
-	// { b=a; *a++; *b; }
-	// --a
-	// a--
-	// *a--
-	// a + 5
-	// 5 + a
-	// a - 5
-	// 5 - a
-	// a < b
-	// a > b
-	// a <= b
-	// a >= b
-	// a += n
-	// a -= n
-	// a[n]
-// }
-// Where X is an iterator type, a and b are objects of this iterator type, t is an object of the type pointed by the iterator type, and n is an integer value.
-
 void test_vec_42()
 {
 	ft::vector<Buffer> vector_buffer;
